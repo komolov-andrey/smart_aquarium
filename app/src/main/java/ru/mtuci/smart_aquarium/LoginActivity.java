2b2d8a60484afbc4cb776aa2dc8efb8726fcb3b0
komolov-andrey <komolov.1995@gmail.com>
2018-04-01 21:54:08 +0300

    start project

---
+++ app/src/main/java/ru/mtuci/smart_aquarium/LoginActivity.java
@@ -0,0 +1,44 @@
+package ru.mtuci.smart_aquarium;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends Activity {
+
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    //сообщение
+    Toast toast = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Обработчик кнопки
+                toast = Toast.makeText(getApplicationContext(),"Пора покормить кота!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+}
+

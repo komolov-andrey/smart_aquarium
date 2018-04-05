e991bb03f9315aed354b43ac8947a3fa1c64b344
komolov-andrey <komolov.1995@gmail.com>
2018-04-05 22:55:00 +0300

    start makeing visualization intervals_feeding, but i could not get id item in fragment (parameters)

---
+++ app/src/main/java/ru/mtuci/smart_aquarium/FeedingIntervals.java
@@ -0,0 +1,24 @@
+package ru.mtuci.smart_aquarium;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class FeedingIntervals extends Activity {
+
+    private Toolbar toolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_feeding);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        toolBar = (Toolbar) findViewById(R.id.my_toolbar_feeding);
+        toolBar.inflateMenu(R.menu.menu);
+    }
+}
--- app/src/main/java/ru/mtuci/smart_aquarium/MainActivity.java
+++ app/src/main/java/ru/mtuci/smart_aquarium/MainActivity.java
@@ -1,21 +1,43 @@
 package ru.mtuci.smart_aquarium;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import ru.mtuci.smart_aquarium.adpter.TabsFragmentAdapter;
 
 public class MainActivity extends FragmentActivity {
 
     private ViewPager viewPager;
+    private Toast toast;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initTabs();
+
+        setContentView(R.layout.parameter);
+
+        TextView feeding_intervals = (TextView) findViewById(R.id.feeding_intervals);
+
+        feeding_intervals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, FeedingIntervals.class);
+                    startActivity(intent);
+                }catch(Exception ex) {
+                    toast = Toast.makeText(getApplicationContext(), "Ошибка при переходе", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }
 
     private void initTabs() {

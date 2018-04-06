0ce745376d18b91fca798dc7e584e43cce4cef73
komolov-andrey <komolov.1995@gmail.com>
2018-04-06 19:24:05 +0300

    make visualization on activity_feeding

--- app/src/main/java/ru/mtuci/smart_aquarium/FeedingIntervals.java
+++ app/src/main/java/ru/mtuci/smart_aquarium/FeedingIntervals.java
@@ -2,7 +2,11 @@ package ru.mtuci.smart_aquarium;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class FeedingIntervals extends Activity {
 
@@ -15,6 +19,27 @@ public class FeedingIntervals extends Activity {
         setContentView(R.layout.activity_feeding);
 
         initToolBar();
+
+        ActionMenuItemView imageButton = (ActionMenuItemView) toolBar.findViewById(R.id.check);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Сохранено", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        FloatingActionButton plusButton = (FloatingActionButton) findViewById(R.id.flab);
+
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Добавлено", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
     }
 
     private void initToolBar() {
